baa7b5bc4680b2420c510682e7a7620d571ee34e
jordanhunt20 <jordanhunt20@gmail.com>
2015-10-19 16:34:13 -0400

    i

---
+++ cps353ms4/drop_tables.sql
@@ -0,0 +1,13 @@
+DROP TABLE [dbo].[CartPromotions]
+
+DROP TABLE [dbo].[CartProducts]
+
+DROP TABLE [dbo].[Carts]
+
+DROP TABLE [dbo].[Promotions]
+
+DROP TABLE [dbo].[Products]
+
+DROP TABLE [dbo].[Categories]
+
+DROP TABLE [dbo].[Users]
